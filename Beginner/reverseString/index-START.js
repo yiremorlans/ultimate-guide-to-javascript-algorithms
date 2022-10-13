7ce28b36262b1adfceda5559cded9ba754983c3d
Yire Morlans <yire.m@icloud.com>
2022-10-12 22:03:10 -0400

    reverseString tests

--- Beginner/reverseString/index-START.js
+++ Beginner/reverseString/index-START.js
@@ -3,12 +3,44 @@ Given a string of text, write an algorithm that returns the text received in a r
 E.g reverseString('algorithms') // should return 'smhtirogla'
 */
 
-
+//
 
 function reverseString(text) {
-    // Code goes here
+    return text.split('').reverse().join('')
 }
 
+// let reverseString = text => [...text].reverse().join('')
+
+
+function reverseString(text){
+    let result = ''
+    for (let i = text.length - 1; i >= 0; i--){
+        result += text[i]
+    }
+    return result
+}
+
+function reverseString(text) {
+    let result = ''
+    for (let letter of text) {      // word: algorithm 
+        result = letter + result // first iteration -> a 
+                                                // -> l a
+                                                // -> g l a
+    }
+}
+
+function reverseString(text) {
+    if (!text) {
+        return ''
+    } else {
+        return reverseString(text.substr(1)) + text[0]
+
+    }
+}
+
+function reverseString(text){
+    return  [...text].reduce((total, letter) => letter + total, '')
+}
 
 
 module.exports = reverseString
