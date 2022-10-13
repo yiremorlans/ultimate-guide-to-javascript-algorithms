5dcfe72546a5df1cb76efc4dd198e363c492b015
Yire Morlans <yire.m@icloud.com>
2022-10-13 17:48:06 -0400

    vowelsCounter complete

--- Beginner/vowelsCounter/index-START.js
+++ Beginner/vowelsCounter/index-START.js
@@ -5,9 +5,31 @@ e.g vowelsCounter('anehizxcv') // will return 3
 
 
 function vowelsCounter(text) {
-    // Code goes here
+    let count = 0
+   for (let i=0; text.length; i++){
+    if ('aeiou'.includes(text[i])) {
+        count ++
+    }
+   }
+   return count
 }
 
+function vowelsCounter(text) {
+    const vowels = ['a', 'e', 'i', 'o', 'u']
+    let counter = 0
+   for (letter of text.toLowerCase()){
+    if (vowels.includes(letter)) {
+        counter ++
+    }
+   }
+   return counter
+}
+
+
+function vowelsCounter(text) {
+   let matchVowels = text.match(/[aeiou]/gi)
+   return (!matchVowels) ? 0 : matchVowels.length
+}
 
 
 module.exports = vowelsCounter;
