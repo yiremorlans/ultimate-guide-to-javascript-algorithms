f0e7db875f78f541981b103a1bdbe83cf1b807fa
Yire Morlans <yire.m@icloud.com>
2022-10-18 11:41:25 -0400

    palindromeChecker

--- Beginner/palindromeChecker/index-START.js
+++ Beginner/palindromeChecker/index-START.js
@@ -7,7 +7,27 @@ e.g palindromeChecker('racecar') // will return true
 
 
 function palindromeChecker(text) {
-    v// Code goes here
+    const reversedText = text.toLowerCase().split('').reverse().join('')
+    return reversedText === text
+}
+
+function palindromeChecker(text) {
+    let charArray = text.toLowerCase().split('')
+
+    let result = charArray.every((letter, index) => {
+        return letter === charArray[charArray.length - index -1]      
+    })
+    return result 
+}
+
+function palindromeChecker(text) {
+    let textLength = text.length
+    for (let i=0; i< textLength/2; i++){
+        if (text[i] !== text[textLength - 1 - i]){
+            return false // if letter does not match, returns false
+        }
+    }
+    return true // else it ran through loop without returning false
 }
 
 
