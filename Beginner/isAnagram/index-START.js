f5cb0dbcde56cebf23f9b915d83bcf0b34354758
Yire Morlans <yire.m@icloud.com>
2022-10-19 16:42:59 -0400

    isAnagram with two solutions

--- Beginner/isAnagram/index-START.js
+++ Beginner/isAnagram/index-START.js
@@ -6,10 +6,58 @@ don't. E.g
 */
 
 
-
 function isAnagram(stringA, stringB) {
-    // Code goes here
+
+    function createCharMap(text) {
+        let charMap= {}
+
+        for (let letter of text) {
+            if (!charMap.hasOwnProperty(letter)){
+                charMap[letter] = 1
+            } else {
+                charMap[letter] += 1
+            }
+        }
+        return charMap
+    }
+
+    if (stringA.length === stringB.length) {
+        let stringMapA = createCharMap(stringA)
+        let stringMapB = createCharMap(stringB)
+
+        for (let value in stringMapA) {
+            if (stringMapA[value] != stringMapB[value]) {
+                return false
+            } 
+        }
+        return true
+    } else {
+        return false
+    }
+    
 }
 
+// make a str sanitizing function to call on stringA and stringB as to not repeat oneself
+function isAnagram(stringA, stringB) {
+    let nonLetters = [' ', '!', '?', '.', ',', '"']
+    let transformA = stringA.split('').map(value => value.toLowerCase()).filter(value => {
+        if (!nonLetters.includes(value)) {
+            return value
+        }
+    }).sort()
+    let transformB = stringB.split('').map(value => value.toLowerCase()).filter(value => {
+        if (!nonLetters.includes(value)) {
+            return value
+        }   
+    }).sort()
+
+	return transformA.join('') === transformB.join('')
+}
+
+// receiving two strings, could have spaces and other characters in them
+//return true or false
+// sanitize the strs by transforming to lowercase
+// remove any chars that are not letters
+// sort the strs and compare
 
 module.exports = isAnagram
