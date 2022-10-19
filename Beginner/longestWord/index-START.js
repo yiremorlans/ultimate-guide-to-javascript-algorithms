0a35e60361e5f94df841cfbde62bec4879cbb47e
Yire Morlans <yire.m@icloud.com>
2022-10-19 14:02:48 -0400

    longestWord solution by for loop, reduce, and sort

--- Beginner/longestWord/index-START.js
+++ Beginner/longestWord/index-START.js
@@ -1,13 +1,50 @@
 /* CHALLENGE
-Given a string of text, write an algorithm that returns the text received in a reversed format. 
-E.g reverseString('algorithms') // should return 'smhtirogla'
+Given a sentence, return the longest word in the string. 
+longestWord('Top Shelf Web Development Training on Scotch') --> should return 'Development'
 */
 
 
 
 function longestWord(text) {
-    // Code goes here
+
+    let maxValue = 0
+    let longestWord = ''
+    let splitText = text.split(' ')
+
+    for (let word of splitText) {
+        if (word.length > maxValue) {
+            maxValue = word.length
+            longestWord = word
+        }
+    }
+    return longestWord
 }
 
+function longestWord(text) {
+    
+	let splitText = text.split(' ')
+	return splitText.sort((a, b) => b.length - a.length)[0]
+}
+
+
+function longestWord(text) {
+
+    let splitText = text.split(' ')
+    return splitText.reduce((total, value) => {
+        if (value.length > total.length) {
+            return value
+        } else {
+            return total
+        }
+    }, '')
+}
+
+// receiving a sentence in a str
+//returning the longest word in the string
+//set a var for longest word and maxvalue = 0
+//iterate through each word
+// use .length of word to set the maxValue
+// store word if above conditon is true
+//return the longestword var
 
 module.exports = longestWord
