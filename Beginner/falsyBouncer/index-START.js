83f23bb1242c33b463ee0fc594b218ef76521fd1
Yire Morlans <yire.m@icloud.com>
2022-10-22 15:37:35 -0400

    falsyBouncer solution w/filter using boolean

--- Beginner/falsyBouncer/index-START.js
+++ Beginner/falsyBouncer/index-START.js
@@ -7,8 +7,14 @@ E.g  falsyBouncer([1, 0, null, '', 5]) // should return [1,5]
 
 
 function falsyBouncer(array) {
-    // Code goes here
+	return array.filter(value => value)
 }
+
+//receiving an array with multiple values of different types 
+//returning a new array with only truthy values
+//declare an empty arr
+//filter through current array by using boolean to evaluate each value
+
 
 
 module.exports = falsyBouncer
