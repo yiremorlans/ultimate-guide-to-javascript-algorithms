b2d752bab1b77697e86f4c79122a17925c9473ec
Yire Morlans <yire.m@icloud.com>
2022-10-22 14:42:04 -0400

    chunkArray solution brute force

--- Beginner/chunkArray/index-START.js
+++ Beginner/chunkArray/index-START.js
@@ -1,12 +1,14 @@
 /* 
- Given two or more arrays, write a function that combines
- their elements into one array without any repetition. 
- E.g  mergeArrays([1,2,3,3,3], [1,4,5,2]) // should return [1,2,3,4,5]
+Given an array and a chunk size, divide the array into many sub-arrays where each sub-array has the specified length. E.g
+chunkArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], 5) should return [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13]]
 */
 
 function chunkArray(array, size) {
-    // Code goes here
+	let newChunk = []
+	for (let i = 0; i < array.length; i += size) {
+		newChunk.push(array.slice(i, i + size))
+	}
+			return newChunk
 }
-
 
 module.exports = chunkArray
